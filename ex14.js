f5e655499f4f59e9fdc9b97db73ffe69de7a174b
aakter3 <121882119+aakter3@users.noreply.github.com>
2023-01-06 11:10:23 -0500

    Add files via upload

---
+++ ex14.js
@@ -0,0 +1,27 @@
+//branches and conditionals
+// let x = document.getElementById('input').value;
+// let y = prompt("enter a number: ");
+// console.log(x);
+// console.log(y);
+
+
+console.log(show);
+//Task1. Write a JavaScript code that will check a variable age. Prompt the age from the user. 
+//If itâ€™s greater than 18 output to the console â€œOld Enoughâ€, otherwise output â€œToo Young!â€. 
+//Use console.log() for this question.
+
+// let age = prompt("Enter your age: ");
+// console.log(age);
+
+//Task2. Write a JavaScript code that will check the variable temperature. 
+
+// Temperature range    |	Text
+// Temp < 32	        | Freezing weather
+// Between 32 and 41	| Very Cold weather
+// Between 42 and 51	| Cold weather
+// Between 52 and 61	| Itâ€™s nice today!
+// Above 61	            | Itâ€™s warm today!
+
+// let weather = document.getElementById("temp").value;
+// console.log(weather);
+// document.getElementById("weather").innerText = "weather";
